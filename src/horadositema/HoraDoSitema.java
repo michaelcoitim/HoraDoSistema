4098aa8b278fcf95e0726fef2ed34c6dc39677fa
Michael Robert Coitim <michaelcoitim1@gmail.com>
2021-03-20 19:17:35 -0300

    primeira versão programa, primeiro commit

---
+++ src/horadositema/HoraDoSitema.java
@@ -0,0 +1,20 @@
+
+package horadositema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author michael coitim
+ */
+public class HoraDoSitema {
+
+    
+    public static void main(String[] args) {
+       Date relogio = new Date();
+       
+        System.out.println("A hroa do sistema é:");
+        System.out.println(relogio.toString());
+    }
+    
+}
